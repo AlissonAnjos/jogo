7f774b950ec105d6130b37176c6df6d13e676ad0
GambiarraMaker <alissontsukuyomi@gmail.com>
2018-07-23 01:05:48 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,47 +1,48 @@
 
+var acel = 2;
 var pos = {
-  x: 0,
-  y: 0
+  x:100,
+  y:100
+}
+var vel = {
+  x:0,
+  y:0
 };
-var dir = 5;
-var diry = 5;
-var cor = {
-  r: 0,
-  g: 0,
-  b: 0
-};
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    vel.x=-acel;
+    vel.y=0;
+  }
+  if (keyCode === RIGHT_ARROW) {
+    vel.x=acel;
+    vel.y=0;
+  }
+  if (keyCode === UP_ARROW) {
+    vel.y=-acel;
+    vel.x=0;
+  }
+  if (keyCode === DOWN_ARROW) {
+    vel.y=acel;
+    vel.x=0;
+  }
+}
+
+function preload(){
+  planoDeFundo = loadImage('espaco.jpg');
+  naveImg = loadImage('nave.png');
+}
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-
+  createCanvas(900,420);
 }
 
 function draw() {
   background(0);
-  cor.r = random(0,255);
-  cor.g = random(0,255);
-  cor.b = random(0,255);
-  fill(cor.r,cor.g,cor.b);
-  stroke(255);
-  if(pos.x > width){
-    dir = -5;
+  image(planoDeFundo,0,0);
+  image(naveImg,pos.x,pos.y);
 
-  }
-  if(pos.x < 0){
-    dir = 5;
-
-  }
-  if(pos.y > height){
-    diry = -5;
-
-  }
-  if(pos.y < 0){
-    diry = 5;
-
-  }
-    pos.x+=dir;
-    pos.y+=diry;
-
-  ellipse(pos.x, pos.y, 50, 50);
+  pos.x+=vel.x;
+  pos.y+=vel.y;
 
 }
